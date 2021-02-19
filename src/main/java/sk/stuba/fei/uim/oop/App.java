6264c83373c62fed55ce6961304c48fcc1611889
Theodora2000 <xsimont@stuba.sk>
2021-02-19 17:54:58 +0100

    Pociatok2

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -9,7 +9,7 @@ public class App {
         System.out.println("Zadaj rok: ");
         Scanner myObj = new Scanner(System.in);
         int rok = myObj.nextInt();
-        int a,b,c,m,n,d,e,marec,april;
+        int a,b,c,m,n,d,e;
         a = rok%19;
         b=  rok%4;
         c= rok%7;
@@ -17,13 +17,31 @@ public class App {
         n=5;
         d=(19*a+m)%30;
         e=(n+2*b+4*c+6*d)%7;
-        marec = 22+d+e;
-        april = d+e-9;
-        if(marec>31) {
-            System.out.println("Apil: " + april);
-        } else{
-            System.out.println("Marec: " + marec);
+
+        if (d+e<=9)
+        {
+            int day = 22+d+e;
+            System.out.println( day + " marca");
         }
+        else if (d==29 && e==6)
+        {
+            System.out.println("19 marec");
+
+        }
+
+        else if (d==28 && e==6 && a>10)
+        {
+            System.out.println("18 april");
+        }
+
+         else
+
+        {
+            int day;
+            day = d+e-9;
+            System.out.println(day+" aprila");
+        }
+
 
 
 
